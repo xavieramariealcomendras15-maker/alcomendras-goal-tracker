96a886aba577a761decfb89189a0efa6a5078ea2
“xavieramariealcomendras15-maker” <xavieramariealcomendras15@gmail.com>
2025-08-22 17:19:38 +0800

    color was changed

--- app/index.jsx
+++ app/index.jsx
@@ -29,7 +29,7 @@ const styles = StyleSheet.create({
   link: {
     marginVertical: 20,
     padding: 16,
-    backgroundColor: '#21cc8d',
+    backgroundColor: '#e3b0e7ff',
     color: 'white',
     borderRadius: 8,
   },
